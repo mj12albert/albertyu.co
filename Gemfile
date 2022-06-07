3afee0ecfda71bc25b9c295b6b4fca05f1a55359
Albert Yu <albert@albertyu.co>
2022-06-07 12:27:48 +0800

    :recycle: v2 refactor

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "https://rubygems.org"
-
-gem "jekyll", "~>2.2"
-gem "sass", "~>3.3"
-gem "compass", "~>1.0.0.alpha.20"
-gem "breakpoint", "~>2.4"
--- source/js/src/functions/insertAfter.js
+++
@@ -1,5 +0,0 @@
-var insertAfter = function(newNode, referenceNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-module.exports = insertAfter;
