d91e4f0a7e23b6416ad37bf30bc1661e30b87cd1
Albert Yu <albert@albertyu.co>
2022-06-09 14:46:50 +0800

    :construction: nextjs csp headers

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const CSP_DIRECTIVES = `
+  default-src 'self';
+  script-src 'self';
+  script-src-elem 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  img-src 'self' data:;
+`
+
+module.exports = {
   reactStrictMode: true,
 
   env: {
     IMAGE_TAG_V: process.env.IMAGE_TAG_V,
   },
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: CSP_DIRECTIVES.replace(/\s{2,}/g, ' ').trim()
+          },
+        ],
+      }
+    ]
+  }
+}
